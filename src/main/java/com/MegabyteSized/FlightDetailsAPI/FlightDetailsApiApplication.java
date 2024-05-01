3fbbbe2a64a9aec5bad25ea9a881c15f280653c6
Nishad Ghogre <ghogrenishad07@gmail.com>
2024-05-01 19:50:15 +0530

    Import project

---
+++ src/main/java/com/MegabyteSized/FlightDetailsAPI/FlightDetailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.MegabyteSized.FlightDetailsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightDetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightDetailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MegabyteSized/FlightDetailsAPI/FlightDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MegabyteSized.FlightDetailsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
